17aedec49b2c2c9c8634f611c5e26df45ffa302f
Gomp3wap1 <163153492+Gomp3wap1@users.noreply.github.com>
2024-10-12 18:16:02 +0800

    Update categories.tsx

--- src/utils/categories.tsx
+++ src/utils/categories.tsx
@@ -13,7 +13,7 @@ import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
 export const categories = [
-  { name: 'Live Global News', icon: <HomeIcon /> },
+  { name: 'Babymetal', icon: <HomeIcon /> },
   { name: 'Coding', icon: <CodeIcon /> },
   { name: 'ReactJS', icon: <CodeIcon /> },
   { name: 'NextJS', icon: <CodeIcon /> },
